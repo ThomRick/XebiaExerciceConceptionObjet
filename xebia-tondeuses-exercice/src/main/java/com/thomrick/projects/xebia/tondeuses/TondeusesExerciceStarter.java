7e8b3984a2c607a48026851bbc0e251ba947845d
thoma <thoma@DESKTOP-636L9PM>
2016-08-05 18:55:41 +0200

    Développement du lancement du programme par lecture des données d'entrée en ligne de commande

--- xebia-tondeuses-exercice/src/main/java/com/thomrick/projects/xebia/tondeuses/TondeusesExerciceStarter.java
+++ xebia-tondeuses-exercice/src/main/java/com/thomrick/projects/xebia/tondeuses/TondeusesExerciceStarter.java
@@ -1,9 +1,46 @@
 package com.thomrick.projects.xebia.tondeuses;
 
+import java.io.IOException;
+
+import com.thomrick.projects.xebia.tondeuses.consummers.IBatchConsummer;
+import com.thomrick.projects.xebia.tondeuses.consummers.impl.BatchConsummer;
+
+/**
+ * TondeusesExerciceStarter.java
+ *
+ * @author ThomRick
+ * @date 2016-08-04
+ *
+ */
 public class TondeusesExerciceStarter {
 
-	public static void main(String[] args) {
-
+	/**
+	 * main
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] filePaths) throws IOException {
+		IBatchConsummer batchConsummer = new BatchConsummer();
+		for (String filePath : filePaths) {
+			String rapport = batchConsummer.run(filePath);
+			printRapport(filePath, rapport);
+		}
+	}
+	
+	/**
+	 * printRapport
+	 * 
+	 * @param filePath
+	 * @param rapport
+	 */
+	private static void printRapport(String filePath, String rapport) {
+		System.out.println("Rapport d'execution:\n");
+		System.out.println("	- fichier d'entree:");
+		System.out.println(filePath + "\n");
+		System.out.println("	- rapport de sortie:");
+		System.out.println(rapport);
+		System.out.println("-------------------------------------");
 	}
 
 }
